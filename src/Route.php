8b478d4889c30e17c47c6c441e7440c1c9f0e86e
Mohammed Taha [BMT] <bmt.mohammedtaha@gmail.com>
2023-06-02 13:08:49 +0200

    Update Route.php

--- src/Route.php
+++ src/Route.php
@@ -4,30 +4,32 @@ declare(strict_types=1);
 
 namespace Effectra\Router;
 
-use Effectra\Http\Contracts\RouterDispatcher;
+use Effectra\Contracts\Http\RouterDispatcher;
 
 /**
- * @method   middleware(string|MiddlewareInterface $middlewareClass): self
+ * @method \Effectra\Router\Middleware  middleware(string|MiddlewareInterface $middlewareClass): self
  * 
- * @method   group(string $common_route, $controller, array $methods): self
- * @method   crud(string $route, $controller, string $actions): self
- * @method   setPreRoute(string $preRoute): void
- * @method   get(string $pattern, array|callable $callback): self
- * @method   post(string $pattern, array|callable $callback): self
- * @method   put(string $pattern, array|callable $callback): self
- * @method   delete(string $pattern, array|callable $callback): self
- * @method   patch(string $pattern, array|callable $callback): self
- * @method   options(string $pattern, array|callable $callback): self
- * @method   any(string $pattern, array|callable $callback): self
- * @method   register(string $method, string $pattern, array|callable $callback): self
- * @method   routes(): array
- * @method   getArguments(string $route_parts, string $pattern_parts): array
+ * @method  \Effectra\Router\Utils group(string $common_route, $controller, array $methods): self
+ * @method  \Effectra\Router\Utils crud(string $route, $controller, string $actions): self
+ * @method  \Effectra\Router\Utils auth(string $pattern, $controller): self
  * 
- * @method addArguments(array $args): void
- * @method addRequest(RequestInterface $request): void
- * @method addResponse(ResponseInterface $response): void
- * @method setNotFound(callable $response): void
- * @method setInternalServerError(callable $response): void
+ * @method  \Effectra\Router\Register setPreRoute(string $preRoute): void
+ * @method  \Effectra\Router\Register get(string $pattern, array|callable $callback): self
+ * @method  \Effectra\Router\Register post(string $pattern, array|callable $callback): self
+ * @method  \Effectra\Router\Register put(string $pattern, array|callable $callback): self
+ * @method  \Effectra\Router\Register delete(string $pattern, array|callable $callback): self
+ * @method  \Effectra\Router\Register patch(string $pattern, array|callable $callback): self
+ * @method  \Effectra\Router\Register options(string $pattern, array|callable $callback): self
+ * @method  \Effectra\Router\Register any(string $pattern, array|callable $callback): self
+ * @method  \Effectra\Router\Register register(string $method, string $pattern, array|callable $callback): self
+ * @method  \Effectra\Router\Register routes(): array
+ * @method  \Effectra\Router\Register getArguments(string $route_parts, string $pattern_parts): array
+ * 
+ * @method \Effectra\Router\Dispatcher addArguments(array $args): void
+ * @method \Effectra\Router\Dispatcher addRequest(RequestInterface $request): void
+ * @method \Effectra\Router\Dispatcher addResponse(ResponseInterface $response): void
+ * @method \Effectra\Router\Dispatcher setNotFound(callable $response): void
+ * @method \Effectra\Router\Dispatcher setInternalServerError(callable $response): void
  */
 
 class Route implements RouterDispatcher

e6ce818187d1bd4a33aa04e79189ea35b53efcbb
BMTmohammedtaha <bmt.mohammedtaha@gmail.com>
2023-12-01 16:09:05 +0100

    using Resolver class to get instance of the resolved class.

--- src/Callback.php
+++ src/Callback.php
@@ -4,27 +4,14 @@ declare(strict_types=1);
 
 namespace Effectra\Router;
 
-use Exception;
+use Effectra\Router\Exception\InvalidCallbackException;
 
 /**
  * The Callback class provides methods to retrieve callback functions from various action formats.
  */
 class Callback
 {
-    protected $container = null;
 
-    protected bool $isUseContainer = false;
-
-    /**
-     * set router container for binding and injected dependencies of controller class
-     * @param $container
-     * @return void
-     */
-    public function setContainer($container): void
-    {
-        $this->container = $container;
-        $this->isUseContainer = true;
-    }
     /**
      * Get the callback for the given action.
      *
@@ -60,9 +47,7 @@ class Callback
     {
         if (strpos($action, '@')) {
             $closure = explode('@', $action);
-            $class = $closure[0];
-            $method = $closure[1];
-            return $this->closure([$class, $method]);
+            return $this->closure($closure);
         }
 
         return function ($action) {
@@ -77,51 +62,23 @@ class Callback
      *
      * @return callable The closure callback.
      *
-     * @throws Exception If the class or method does not exist.
+     * @throws InvalidCallbackException If the class or method does not exist.
      */
     public function closure(array $action): callable
     {
         [$class, $method] = $action;
 
         if (class_exists($class)) {
-            if ($this->isUseContainer) {
-                $reflection = new \ReflectionClass($class);
-                $constructor = $reflection->getConstructor();
-                if ($constructor) {
-                    // Get the parameters of the constructor
-                    $parameters = $constructor->getParameters();
-                    $dependencies = [];
-                    foreach ($parameters as $parameter) {
-                        $parameterType = $parameter->getType();
 
-                        // Check if the parameter has a type
-                        if ($parameterType !== null) {
-                            // Get the name of the dependency class
-                            $dependencyClass = $parameterType->getName();
-
-                            // Resolve the dependency instance from the container
-                            $dependencyInstance = $this->container->get($dependencyClass);
-
-                            // Add the dependency instance to the array
-                            $dependencies[] = $dependencyInstance;
-                        }
-                    }
-                    $classInstance = new $class(...$dependencies);
-                }
-                else {
-                    $classInstance = new $class();
-                }
-            } else {
-                $classInstance = new $class();
-            }
+            $classInstance = Resolver::resolveClass($class);
 
             if (method_exists($classInstance, $method)) {
                 return [$classInstance, $method];
             } else {
-                throw new Exception("Method not found: {$class}::{$method}");
+                throw new InvalidCallbackException("Method not found: {$class}::{$method}");
             }
         } else {
-            throw new Exception("Controller class {$class} not found");
+            throw new InvalidCallbackException("Controller class {$class} not found");
         }
     }
 
@@ -132,20 +89,20 @@ class Callback
      *
      * @return callable The callback function.
      *
-     * @throws Exception If the class is not found or does not have an __invoke or index method.
+     * @throws InvalidCallbackException If the class is not found or does not have an __invoke or index method.
      */
     public function class(string $class): callable
     {
         if (class_exists($class)) {
             if (method_exists($class, '__invoke')) {
-                return new $class();
+                return Resolver::resolveClass($class)();
             } elseif (method_exists($class, 'index')) {
                 return $this->closure([$class, 'index']);
             } else {
-                throw new Exception("Class '{$class}' does not have an __invoke or index method.");
+                throw new InvalidCallbackException("Class '{$class}' does not have an __invoke or index method.");
             }
         } else {
-            throw new Exception("Class '{$class}' not found.");
+            throw new InvalidCallbackException("Class '{$class}' not found.");
         }
     }
 }

39548bb306ceebc9b97699707e474d05582b4b14
BMTmohammedtaha <bmt.mohammedtaha@gmail.com>
2023-12-12 17:23:16 +0100

    remove methods docBlock

--- src/Route.php
+++ src/Route.php
@@ -4,38 +4,17 @@ declare(strict_types=1);
 
 namespace Effectra\Router;
 
-use Effectra\Contracts\Http\RouterDispatcher;
 use Psr\Container\ContainerInterface;
 
 /**
- * @method  \Effectra\Router\Middleware middleware(string|MiddlewareInterface $middlewareClass): self
- * 
- * @method  \Effectra\Router\Utils group(string $common_route, $controller, array $methods): self
- * @method  \Effectra\Router\Utils crud(string $route, $controller, string $actions, ?MiddlewareInterface|array $middleware = null): self
- * @method  \Effectra\Router\Utils auth(string $pattern, $controller): self
- * 
- * @method  \Effectra\Router\Register setPreRoute(string $preRoute): void
- * @method  \Effectra\Router\Register get(string $pattern, array|callable $callback): self
- * @method  \Effectra\Router\Register post(string $pattern, array|callable $callback): self
- * @method  \Effectra\Router\Register put(string $pattern, array|callable $callback): self
- * @method  \Effectra\Router\Register delete(string $pattern, array|callable $callback): self
- * @method  \Effectra\Router\Register patch(string $pattern, array|callable $callback): self
- * @method  \Effectra\Router\Register options(string $pattern, array|callable $callback): self
- * @method  \Effectra\Router\Register any(string $pattern, array|callable $callback): self
- * @method  \Effectra\Router\Register register(string $method, string $pattern, array|callable $callback): self
- * @method  \Effectra\Router\Register routes(): array
- * @method  \Effectra\Router\Register name(): self
- * @method  \Effectra\Router\Register getArguments(string $route_parts, string $pattern_parts): array
- * 
- * @method \Effectra\Router\Dispatcher addArguments(array $args): void
- * @method \Effectra\Router\Dispatcher addRequest(ServerRequestInterface $request): void
- * @method \Effectra\Router\Dispatcher addResponse(ResponseInterface $response): void
- * @method \Effectra\Router\Dispatcher setNotFound(callable $response): void
- * @method \Effectra\Router\Dispatcher setInternalServerError(callable $response): void
+ * class Route
+ * a lightweight and flexible routing library for PHP applications. It provides a convenient way to define routes and dispatch incoming requests to the appropriate controllers or callbacks.
+ * @package  Effectra\Router
  */
-
 class Route
 {
+    use Dispatcher, Register, Middleware, Utils;
+    
     /**
      * Set the dependency injection container for the Resolver class.
      *
@@ -48,5 +27,4 @@ class Route
         Resolver::setContainer($container);
     }
 
-    use Dispatcher, Register, Middleware, Utils;
 }
